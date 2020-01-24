62c48134dd0ec994ecc8680e340046d971c6f790
Pavel Novichkov <pnovichkov@gmail.com>
2020-01-23 20:01:04 -0800

    Stack exmaple

--- src/ds/_Stack.java
+++ src/ds/_Stack.java
@@ -1,5 +1,29 @@
 package ds;
 
+import ds.node.Node;
+
 public class _Stack {
+	Node top;
+	
+	public void push(int data) {
+		Node node = new Node(data);
+		node.next = top;
+		top = node;
+	}  
+	
+	public boolean isEmpty() {
+		return top == null;
+	}
+	
+	public int peek() {
+		if(top == null) throw new RuntimeException("Stack is empty");
+		return top.data;
+	}
+	
+	public int pop() {
+		int data = peek();
+		top = top.next;
+		return data;
+	}
 
 }
