f8dccfd4eb0acd7bb5c7258b5d3b6178153460a4
georgy <georgezakharov@yahoo.com>
2020-01-28 12:32:03 -0500

    second attempt at Stack -G

---
+++ src/ds/G_Stack_02.java
@@ -0,0 +1,59 @@
+package ds;
+
+import ds.node.Node;
+
+/**
+ * start : 12:28
+ * end   : 12:31
+ *
+ * @author georgezakharov
+ */
+
+
+public class G_Stack_02 {
+
+    Node top;
+
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+
+    public int peek() {
+
+        if (isEmpty()) throw new RuntimeException("Stack Empty");
+
+        return top.data;
+    }
+
+    public int pop() {
+
+        int data = peek();
+
+        if (top.next == null) {
+
+            top = null;
+        } else {
+            top = top.next;
+        }
+
+        return data;
+    }
+
+    public void push(int data){
+        Node node = new Node(data);
+
+        if(isEmpty()){
+            top = node;
+        }
+        else{
+
+            top.next = top;
+            top = node;
+        }
+
+    }
+
+
+}
