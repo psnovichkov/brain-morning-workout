78486ba87c78ce9e575cce06b0c712d205051d43
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-01-30 18:09:33 -0800

    [Andrey] Added queue and stack implementations

---
+++ src/ds/A_Stack_01.java
@@ -0,0 +1,29 @@
+package ds;
+import ds.node.Node;
+
+public class A_Stack_01 {
+
+	Node top;
+	
+	public boolean isEmpty() {
+		return top == null;
+	}
+		
+	public int peek() {
+		if(isEmpty()) throw new RuntimeException("Stack is empty");
+		return top.data;
+	}
+	
+	public int poll() {
+		int output = peek();
+		top = top.next;
+		return output;
+	}
+	
+	public void push(int value) {
+		Node newNode = new Node(value);
+		newNode.next = top;
+		top = newNode;
+	}
+	
+}
