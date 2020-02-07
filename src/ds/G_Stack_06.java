e809b6e6309cf1de79bbf13931d159c91845b8d9
georgy <georgezakharov@yahoo.com>
2020-02-06 21:19:38 -0500

    updates

---
+++ src/ds/G_Stack_06.java
@@ -0,0 +1,35 @@
+package ds;
+
+import ds.node.Node;
+
+public class G_Stack_06 {
+
+    Node top;
+
+    public boolean isEmpty() {
+        return top == null;
+
+    }
+
+    public int peek() {
+        if (top == null) throw new RuntimeException("Stack is Empty");
+        return top.data;
+    }
+
+    public int pop() {
+
+        int data = peek();
+
+        top = top.next;
+        return data;
+    }
+
+    public void push(int data) {
+        Node node = new Node(data);
+
+        node.next = top;
+        top = node;
+    }
+
+
+}
