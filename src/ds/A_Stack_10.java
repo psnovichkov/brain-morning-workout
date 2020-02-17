650db1b596323aeee54a36befce78fee8f59574d
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-02-17 07:58:41 -0800

    [add] More stack implementations

---
+++ src/ds/A_Stack_10.java
@@ -0,0 +1,30 @@
+package ds;
+
+import ds.node.Node;
+
+//	Time: 1:13
+
+public class A_Stack_10 {
+	Node top;
+	
+	public boolean isEmpty() {
+		return top==null;
+	}
+	
+	public int peek() {
+		if(isEmpty()) throw new RuntimeException("Stack is empty");
+		return top.data;
+	}
+	
+	public int pop() {
+		int val = top.data;
+		top = top.next;
+		return val;
+	}
+	
+	public void push(int val) {
+		Node node = new Node(val);
+		node.next = top;
+		top = node;
+	}
+}
