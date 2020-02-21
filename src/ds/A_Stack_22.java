d89357d4407791cdc8cb3e6f6d5bcb34024548b1
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-02-21 11:06:21 -0800

    New Stack

---
+++ src/ds/A_Stack_22.java
@@ -0,0 +1,30 @@
+package ds;
+
+import ds.node.Node;
+
+public class A_Stack_22 {
+
+	Node top;
+	
+	public boolean isEmpty() {
+		return top == null;
+	}
+	
+	public int peek() {
+		if(isEmpty()) throw new RuntimeException("Stack is empty");
+		return top.data;
+	}
+	
+	public int pop() {
+		int v = peek();
+		top = top.next;
+		return v;
+	}
+	
+	public void add(int v) {
+		Node node = new Node(v);
+		node.next = top;
+		top = node;
+	}
+	
+}
