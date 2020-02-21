c1e5979f0910d1f0228f83212c7dec3e419fa6d4
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-02-21 14:01:49 -0800

    New implementations

---
+++ src/ds/A_Queue_26.java
@@ -0,0 +1,44 @@
+package ds;
+
+import ds.node.Node;
+
+// Time: 1:15
+
+public class A_Queue_26 {
+
+	Node top;
+	Node bottom;
+	
+	public boolean isEmpty() {
+		return top == null;
+	}
+	
+	public int peek() {
+		if(isEmpty()) throw new RuntimeException("Queue is empty");
+		return top.data;
+	}
+	
+	public void add(int v) {
+		Node node = new Node(v);
+		if(isEmpty()) {
+			top = node;
+			bottom = node;
+		}else {
+			bottom.next = node;
+			bottom = node;
+		}
+	}
+	
+	public int remove() {
+		int v = peek();
+		if(top == bottom) {
+			top = null;
+			bottom = null;
+		}else {
+			top = top.next;
+		}
+		return v;
+	}
+	
+	
+}
