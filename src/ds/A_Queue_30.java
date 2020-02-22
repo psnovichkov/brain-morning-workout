1cc44a9ad8c00178d82acc83e591f06a620888a4
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-02-22 13:25:57 -0800

    [add] More implementations

---
+++ src/ds/A_Queue_30.java
@@ -0,0 +1,44 @@
+package ds;
+
+import ds.node.Node;
+
+//Time: 1:10
+
+public class A_Queue_30 {
+
+	Node top;
+	Node bottom;
+	
+	public boolean isEmpty() {
+		return top == null;
+	}
+	
+	public int peek() {
+		if(isEmpty()) throw new RuntimeException("Queue is emptyu");
+		return top.data;
+	}
+	
+	public int pop() {
+		int v = peek();
+		if(top == bottom) {
+			top = null;
+			bottom = null;
+		}else {
+			top = top.next;
+		}
+		return v;
+	}
+	
+	public void add(int v) {
+		Node node = new Node(v);
+		if(isEmpty()) {
+			top = node;
+			bottom = node;
+		}else {
+			bottom.next = node;
+			bottom = node;
+		}
+	}
+	
+	
+}
