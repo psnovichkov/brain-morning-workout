f79f153919cad4d3bc04455f428854a39f298b48
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-02-26 12:40:27 -0800

    [add] More implementations

---
+++ src/ds/A_Stack_33.java
@@ -0,0 +1,33 @@
+package ds;
+
+import ds.node.Node;
+
+// Time: 0:56
+
+public class A_Stack_33 {
+	
+	Node top;
+	
+	public boolean isEmpty() {
+		return top == null;
+	}
+	
+	public int peek() {
+		if(isEmpty()) throw new RuntimeException("Stack is emptu");
+		return top.data;
+		
+	}
+	
+	public int pop() {
+		int v = peek();
+		top = top.next;
+		return v;
+	}
+	
+	public void push(int v) {
+		Node node = new Node(v);
+		node.next = top;
+		top = node;
+	}
+	
+}
