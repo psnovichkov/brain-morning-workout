8a96d79fbfd74ea0e0099a3577e47cab8e5c5533
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-03-14 12:45:41 -0400

    [add] New implementations

---
+++ src/ds/binary_search_tree/A_BinarySearchTree_17.java
@@ -0,0 +1,48 @@
+package ds.binary_search_tree;
+
+public class A_BinarySearchTree_17 {
+
+	static class Node{
+		int data;
+		int height;
+		
+		Node left;
+		Node right;
+		
+		Node(int data, int height){
+			this.data = data;
+			this.height = height;
+		}
+	}
+	
+	private Node root;
+	
+	public Node search(int data) {
+		return search(root, data);
+	}
+	
+	private Node search(Node node, int data) {
+		if(node == null) return null;
+		if(data < node.data) return search(node.left, data);
+		if(data > node.data) return search(node.right, data);
+		return node;
+	}
+	
+	public void insert(int data) {
+		root = insert(root, data, -1);
+	}
+	
+	private Node insert(Node node, int data, int height) {
+		height++;
+		if(node == null) {
+			node = new Node(data, height);
+		}else {
+			if(data <= node.data) {
+				node.left = insert(node.left, data, height);
+			}else {
+				node.right = insert(node.right, data, height);
+			}
+		}
+		return node;
+	}
+}
