c4abafc3a56dcb684393226d5300f5de9600c551
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-03-28 15:48:11 -0700

    [add] New implementations

---
+++ src/ds/stack/A_Stack_62.java
@@ -0,0 +1,30 @@
+package ds.stack;
+
+import ds.node.Node;
+
+public class A_Stack_62 {
+
+	Node top;
+	
+	public boolean isEmpty() {
+		return top == null;
+	}
+	
+	public int peek() {
+		if(isEmpty()) throw new RuntimeException("Stack is empty");
+		return top.data;
+	}
+	
+	public int pop() {
+		int v = peek();
+		top = top.next;
+		return v;
+	}
+	
+	public void push(int v) {
+		Node node = new Node(v);
+		node.next = top;
+		top = node;
+	}
+	
+}
