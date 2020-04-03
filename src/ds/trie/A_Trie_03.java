aa0e351bf2c38d13d06d92dbf818d80037485bf2
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-04-02 17:59:24 -0700

    [add] New implementatsion

---
+++ src/ds/trie/A_Trie_03.java
@@ -0,0 +1,46 @@
+package ds.trie;
+
+public class A_Trie_03 {
+
+	public static final int MIN_VALUE = 'a';
+	public static final int MAX_VALUE = 'z';
+	public static final int ALPHABET_SIZE = MAX_VALUE - MIN_VALUE + 1;
+	
+	static class Node{
+		Node[] children;
+		boolean isWordEnding;
+	}
+	
+	private Node root = new Node();
+	
+	public void add(String word) {
+		Node node = root;
+		
+		for(char ch: word.toCharArray()) {
+			int index = ch - MIN_VALUE;
+			if(node.children == null) {
+				node.children = new Node[ALPHABET_SIZE];
+			}
+			if(node.children[index] == null) {
+				node.children[index] = new Node();
+			}
+			node = node.children[index];
+		}
+		node.isWordEnding = true;
+	}
+	
+	public boolean contains(String word) {
+		Node node = root;
+		
+		for(char ch: word.toCharArray()) {
+			int index = ch - MIN_VALUE;
+			if(node.children == null) return false;
+			if(node.children[index] == null) return false;
+			node = node.children[index];
+		}
+		
+		return node.isWordEnding;
+	}
+	
+	
+}
