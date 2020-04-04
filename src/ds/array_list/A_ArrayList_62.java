13b2f3414c218067a341092777b5f1d9c3a1f166
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-04-04 14:48:45 -0700

    [add] new implementations

---
+++ src/ds/array_list/A_ArrayList_62.java
@@ -0,0 +1,45 @@
+package ds.array_list;
+
+public class A_ArrayList_62 {
+
+	private final static int DEFAULT_CAPACITY = 100;
+	private final static int EXPAND_FACTOR = 2;
+	
+	private int[] values;
+	private int capacity;
+	private int size;
+	
+	public A_ArrayList_62(int capacity) {
+		this.capacity = capacity;
+		values = new int[capacity];
+		size = 0;
+	}
+	
+	public A_ArrayList_62() {
+		this(DEFAULT_CAPACITY);
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public void add(int v) {
+		ensureCapacity();
+		values[size++] = v;
+	}
+	
+	public int get(int index) {
+		if(index >= size) throw new IndexOutOfBoundsException("Index is out of bounds: " + index);
+		return values[index];
+	}
+	
+	private void ensureCapacity() {
+		if(size == capacity) {
+			capacity *= EXPAND_FACTOR;
+			int[] tmp = new int[capacity];
+			System.arraycopy(values, 0, tmp, 0, size);
+			values = tmp;
+		}
+	}
+	
+}
