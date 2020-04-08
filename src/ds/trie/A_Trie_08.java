cdf63325a17325054c26524a47dd313ce744f759
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-04-08 11:46:02 -0700

    [add] New implementations

---
+++ src/ds/trie/A_Trie_08.java
@@ -0,0 +1,44 @@
+package ds.trie;
+
+public class A_Trie_08 {
+
+	private final static int MIN_VALUE = 'a';
+	private final static int MAX_VALUE = 'z';
+	private final static int ALPHABET_SIZE = MAX_VALUE - MIN_VALUE;
+	
+	static class Node{
+		Node[] children;
+		boolean isWordEnd;
+	}
+	
+	private Node root = new Node();
+	
+	public void add(String word) {
+		Node node = root;
+		for(char ch: word.toCharArray()) {
+			int index = ch - MIN_VALUE;
+			if(node.children == null) {
+				node.children = new Node[ALPHABET_SIZE];
+			}
+			if(node.children[index] == null) {
+				node.children[index] = new Node();
+			}
+			node = node.children[index];
+		}
+		
+		node.isWordEnd = true;
+	}
+	
+	public boolean contains(String word) {
+		Node node = root;
+		for(char ch: word.toCharArray()) {
+			int index = ch - MIN_VALUE;
+			if(node.children == null) return false;
+			if(node.children[index] == null) return false;
+			node = node.children[index];
+		}
+		return node.isWordEnd;
+	}
+	
+	
+}
