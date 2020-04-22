f7c375f387ce6a7b58077595521236d0c8fb10a6
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-04-22 10:47:24 -0700

    [add] New implementations

---
+++ src/alg/traversal/bt_iterative_level/A_Traversal_17.java
@@ -0,0 +1,34 @@
+package alg.traversal.bt_iterative_level;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import alg.traversal.Visitor;
+import ds.node.BTNode;
+
+public class A_Traversal_17 {
+
+	Visitor visitor;
+	
+	public A_Traversal_17(Visitor visitor) {
+		this.visitor = visitor;
+	}
+	
+	public void levelOrder(BTNode root) {
+		Queue<BTNode> q = new LinkedList<BTNode>();
+		q.add(root);
+		
+		while(!q.isEmpty()) {
+			BTNode node = q.remove();
+			visit(node);
+			
+			if(node.left != null) q.add(node.left);
+			if(node.right != null) q.add(node.right);
+		}
+	}
+	
+	private void visit(BTNode node) {
+		visitor.visit(node);
+	}
+	
+}
