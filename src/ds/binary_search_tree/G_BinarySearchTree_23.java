d61d50984559889ae5b6d44803e99728435e0ceb
Georgy Zakharov <soccerzakharov@gmail.com>
2020-04-29 18:14:27 -0400

    G

---
+++ src/ds/binary_search_tree/G_BinarySearchTree_23.java
@@ -0,0 +1,40 @@
+package ds.binary_search_tree;
+
+public class G_BinarySearchTree_23 {
+
+    static class Node {
+        Node left;
+        Node right;
+        int data;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    private Node root;
+
+    public void insert(int data) {
+        root = insert(root, data);
+    }
+
+    private Node insert(Node par, int data) {
+        if (par == null) par = new Node(data);
+        else if (data < par.data) par.left = insert(par.left, data);
+        else if (data > par.data) par.right = insert(par.right, data);
+
+        return par;
+    }
+
+    public Node search(int data) {
+        return search(root, data);
+    }
+
+    private Node search(Node par, int data) {
+        if (par == null) return null;
+        else if (data < par.data) return search(par.left, data);
+        else if (data > par.data) return search(par.right, data);
+
+        return par;
+    }
+}
