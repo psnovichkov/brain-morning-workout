1ef79f39f9d1e90c631e18989b3d546ade3018dc
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-05-14 21:32:36 -0700

    [add] New implementations

---
+++ src/ds/hash_map/A_HashMap_49.java
@@ -0,0 +1,68 @@
+package ds.hash_map;
+
+public class A_HashMap_49<K,V>{
+
+    private final static int DEFAULT_CAPACITY = 100;
+
+    static class Item<K,V>{
+        K key;
+        V value;
+
+        Item<K,V> next;
+
+        public Item(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private Item<K,V>[] buckets;
+    private int size;
+    private int capacity;
+
+    public A_HashMap_49(int capacity) {
+        this.capacity = capacity;
+        buckets = new Item[capacity];
+        size = 0;
+    }
+
+    public A_HashMap_49() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    public int size(){
+        return size;
+    }
+
+    private int getIndex(K key){
+        return key.hashCode()%capacity;
+    }
+
+    public V get(K key){
+        Item<K,V> item = findItem(key);
+        return item != null ? item.value : null;
+    }
+
+    private Item<K,V> findItem(K key){
+        Item<K,V> item = buckets[getIndex(key)];
+        while (item != null) {
+            if(item.key.equals(key)) break;
+            item = item.next;
+        }
+        return item;
+    }
+
+    public void put(K key, V value){
+        Item<K, V> item = findItem(key);
+        if (item == null) {
+            int index = getIndex(key);
+            item = new Item<K,V>(key, value);
+
+            item.next = buckets[index];
+            buckets[index] = item;
+            size++;
+        }else{
+            item.value = value;
+        }
+    }
+}
---
+++ src/ds/min_heap/A_MinHeap_24.java
@@ -0,0 +1,99 @@
+package ds.min_heap;
+
+public class A_MinHeap_24 {
+
+    private final static int DEFAULT_CAPACITY = 100;
+    private final static int EXPAND_FACTOR = 2;
+    private final static int ROOT_INDEX = 1;
+
+    private int[] values;
+    private int capacity;
+    private int size;
+
+    public A_MinHeap_24(int capacity) {
+        this.capacity = capacity;
+        values = new int[capacity];
+        size = 0;
+    }
+
+    public A_MinHeap_24(){
+        this(DEFAULT_CAPACITY);
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public int peek(){
+        if(isEmpty()) throw new RuntimeException("Min Heap is empty");
+        return values[ROOT_INDEX];
+    }
+
+    public void add(int v){
+        ensureCapacity();
+        size++;
+        values[lastIndex()] = v;
+        heapifyUp();
+    }
+
+    public int poll(){
+        int v = peek();
+        values[ROOT_INDEX] = values[lastIndex()];
+        size--;
+        heapifyDown();
+        return v;
+    }
+
+    private void heapifyDown(){
+        int pIndex = ROOT_INDEX;
+        int leftIndex = pIndex * 2;
+        int rightIndex = leftIndex + 1;
+
+        while (validIndex(leftIndex)) {
+            int minIndex = leftIndex;
+            if (validIndex(rightIndex) && values[rightIndex] < values[leftIndex]) {
+                minIndex = leftIndex;
+            }
+
+            if(values[pIndex] < values[minIndex]) break;
+            swap(pIndex, minIndex);
+            pIndex = minIndex;
+            leftIndex = pIndex * 2;
+            rightIndex = leftIndex + 1;
+        }
+    }
+
+    private void heapifyUp(){
+        int cIndex = lastIndex();
+        int pIndex = cIndex / 2;
+
+        while (validIndex(pIndex) && values[pIndex] < values[cIndex]) {
+            swap(cIndex, pIndex);
+            cIndex = pIndex;
+            pIndex = cIndex / 2;
+        }
+    }
+
+    private int lastIndex(){
+        return ROOT_INDEX + size - 1;
+    }
+
+    private boolean validIndex(int index) {
+        return index >= ROOT_INDEX && index <= lastIndex();
+    }
+
+    private void swap(int i, int j){
+        int tmp = values[i];
+        values[i] = values[j];
+        values[j] = tmp;
+    }
+
+    private void ensureCapacity(){
+        if (size == capacity) {
+            capacity *= EXPAND_FACTOR;
+            int[] tmp = new int[capacity];
+            System.arraycopy(values, 0, tmp, 0, size);
+            values = tmp;
+        }
+    }
+}
