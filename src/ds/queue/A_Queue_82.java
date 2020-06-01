69a32123cb89862cf46b142e4c1576df9b6e8a8b
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-06-01 10:28:56 -0700

    [add] New implementations

---
+++ src/ds/queue/A_Queue_82.java
@@ -0,0 +1,40 @@
+package ds.queue;
+
+import ds.node.Node;
+
+public class A_Queue_82 {
+    Node top;
+    Node bottom;
+
+    public boolean isEmpty(){
+        return top == null;
+    }
+
+    public int peek() {
+        if(isEmpty()) throw new RuntimeException("Queue is empty");
+        return top.data;
+    }
+
+    public void add(int v){
+        Node node = new Node(v);
+        if (isEmpty()) {
+            top = node;
+            bottom = node;
+        } else {
+            node.next = bottom;
+            bottom = node;
+        }
+    }
+
+    public int remove() {
+        int v = peek();
+        if (top == bottom) {
+            top = null;
+            bottom = null;
+        } else {
+            top = top.next;
+        }
+        return v;
+    }
+
+}
