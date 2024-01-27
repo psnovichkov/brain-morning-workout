3f5b461bbbe7a0d7de90727ae80026f1dab85bc2
Elena Novichkova <elenanovichkova@gmai.om>
2024-01-27 14:00:17 -0800

    formatted code

--- src/ds/stackjs/E_Stack_01.js
+++ src/ds/stackjs/E_Stack_01.js
@@ -1,29 +1,29 @@
 const Node = function(value){
     this.value = value;
- this.next = null;
+    this.next = null;
 }
 
 const Stack = function (){
-this.top = null
-const isEmpty = function(){
-    return this.top == null;
-}
-const push = function(value){
-    let newNode = new Node(value);
- newNode.next = this.top;
- this.top = newNode;
-}
-const poll = function(){
-    let output = this.peek();
- this.top = this.top.next;
- return output;
-}
-const peek = function(){
-    if(this.isEmpty){
-     throw new Error("stack is empty");
- }
- return this.top.value;
-}
+    this.top = null
+    const isEmpty = function(){
+        return this.top == null;
+    }
+    const push = function(value){
+        let newNode = new Node(value);
+        newNode.next = this.top;
+        this.top = newNode;
+    }
+    const poll = function(){
+        let output = this.peek();
+        this.top = this.top.next;
+        return output;
+    }
+    const peek = function(){
+        if(this.isEmpty){
+        throw new Error("stack is empty");
+        }
+        return this.top.value;
+    }
 }
 
 let stack = new Stack();
