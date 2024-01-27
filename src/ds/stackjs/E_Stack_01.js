7c17ef56b2396d04cb638674f89b2bd7ffdbb19f
Elena Novichkova <elenanovichkova@gmai.om>
2024-01-27 13:55:29 -0800

    stack implementation in javascript

---
+++ src/ds/stackjs/E_Stack_01.js
@@ -0,0 +1,31 @@
+const Node = function(value){
+    this.value = value;
+ this.next = null;
+}
+
+const Stack = function (){
+this.top = null
+const isEmpty = function(){
+    return this.top == null;
+}
+const push = function(value){
+    let newNode = new Node(value);
+ newNode.next = this.top;
+ this.top = newNode;
+}
+const poll = function(){
+    let output = this.peek();
+ this.top = this.top.next;
+ return output;
+}
+const peek = function(){
+    if(this.isEmpty){
+     throw new Error("stack is empty");
+ }
+ return this.top.value;
+}
+}
+
+let stack = new Stack();
+stack.push("3");
+console.log(stack.peek());
