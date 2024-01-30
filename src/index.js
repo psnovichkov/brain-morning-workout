72159d3ef1197ecb0e36490d76bbd674a08c58fd
Elena Novichkova <elenanovichkova@gmai.om>
2024-01-29 19:49:46 -0800

    uncomment parenthesis code

--- src/index.js
+++ src/index.js
@@ -6,36 +6,36 @@ import Queue from "./ds/queue/E_Queue_01.js";
 // stack.push(3);
 // console.log(stack.peek());
 
-// const checkParenthesis = function (str) {
-//   const stack = new Stack();
-//   let parString = str;
-//   const chars = parString.split("");
-//   for (const c of parString) {
-//     if (c == "(") {
-//       const open = "open: (";
-//       stack.push(open);
-//       console.log(open);
-//     }
-//     if (c == ")") {
-//       let polled = stack.poll();
-//       console.log(polled);
-//     }
-//   }
+const checkParenthesis = function (str) {
+  const stack = new Stack();
+  let parString = str;
+  const chars = parString.split("");
+  for (const c of parString) {
+    if (c == "(") {
+      const open = "open: (";
+      stack.push(open);
+      console.log(open);
+    }
+    if (c == ")") {
+      let polled = stack.poll();
+      console.log(polled);
+    }
+  }
 
-//   if (stack.isEmpty()) {
-//     console.log("String of parentheses is valid.");
-//   } else {
-//     console.log("String of parentheses is NOT valid.");
-//   }
-// };
+  if (stack.isEmpty()) {
+    console.log("String of parentheses is valid.");
+  } else {
+    console.log("String of parentheses is NOT valid.");
+  }
+};
 
-// checkParenthesis("((()()())");
-// checkParenthesis("((()()()))");
+checkParenthesis("((()()())");
+checkParenthesis("((()()()))");
 
-const queue = new Queue();
-queue.add(5);
-queue.add(4);
-queue.add(3);
-queue.remove();
+// const queue = new Queue();
+// queue.add(5);
+// queue.add(4);
+// queue.add(3);
+// queue.remove();
 
-console.log(queue.peek());
+// console.log(queue.peek());
